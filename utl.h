629282e16a5182944e3199867526bcf9817db6a8
Eduardo Costa <m4c0@users.noreply.github.com>
2026-05-01 15:14:27 +0100

    reading a vocab table

--- utl.h
+++ utl.h
@@ -2,6 +2,17 @@
 #include <stdio.h>
 #include "mem.h"
 
+typedef struct utl_wstr {
+  const wchar_t * str;
+  int sz;
+} utl_wstr_t;
+static wchar_t * utl_wstr_printable(utl_wstr_t str) {
+  wchar_t * dup = mem_alloc((str.sz + 1) * sizeof(wchar_t));
+  for (int i = 0; i < str.sz; i++) dup[i] = str.str[i] < 0x80 ? str.str[i] : '?';
+  dup[str.sz] = 0;
+  return dup;
+}
+
 static void utl_assert(size_t n, const char * msg, const char * file, int line) {
   if (n) return;
   fprintf(stderr, "%s:%d: Assertion failed: %s\n", file, line, msg);
--- vocab.c
+++ vocab.c
@@ -1,5 +1,32 @@
 #include "utl.h"
 
+#include <string.h>
+#include <wchar.h>
+
+static utl_wstr_t vcb_map[50000] = {0};
+static wchar_t * vcb_mbstowcs(const char * u8, wchar_t * mb) {
+  // This is equivalent to mbstowcs but we don't need to rely on
+  // changing/restoring the multibyte locale
+  for (; *u8; u8++, mb++) {
+    if ((u8[0] & 0x80) == 0) {
+      *mb = *u8;
+      continue;
+    }
+    assert((u8[0] & 0xE0) == 0xC0 && (u8[1] & 0xC0) == 0x80 && "found unsupported char in vocab.bpe");
+    *mb = ((u8[0] & 0x1F) << 6) | (u8[1] & 0x3F);
+    u8++;
+  }
+  return mb;
+}
+static wchar_t * vcb_utf8_to_wchar(const char * a, const char * b) {
+  // Final string will never be greater than original. Since it can be smaller,
+  // we have to clear everything.
+  wchar_t * res = mem_alloc((strlen(a) + strlen(b) + 1) * sizeof(wchar_t));
+  // We concatenate both because the algo here only uses vocab.bpe for ranking.
+  vcb_mbstowcs(b, vcb_mbstowcs(a, res));
+  return res;
+}
+
 int main() {
   char * file = utl_slurp("vocab.json");
 
@@ -18,7 +45,8 @@ int main() {
       }
       file++;
     }
-    *file++ = 0;
+    *file = 0;
+    wchar_t * k = vcb_utf8_to_wchar(key, file++);
 
     assert(*file++ == ':');
 
@@ -26,7 +54,10 @@ int main() {
     while (*file >= '0' && *file <= '9') file++;
     int val = atoi(n);
 
-    printf("%6d %s\n", val, key);
+    vcb_map[val].str = k;
+    vcb_map[val].sz  = wcslen(k);
+
+    printf("%6d %ls\n", val, utl_wstr_printable(vcb_map[val]));
 
     if (*file == '}') break;
     assert(*file++ == ',');
