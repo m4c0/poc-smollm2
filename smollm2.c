f914feb3801a3a4656662a7ab5754dfdb4358c37
Eduardo Costa <m4c0@users.noreply.github.com>
2026-05-03 16:23:04 +0100

    loading input into buffer

--- smollm2.c
+++ smollm2.c
@@ -3,6 +3,29 @@
 
 static const char * txt = "What's the capital of France?";
 
+static vlk_buffer_t load_input(const char * txt, unsigned * sz) {
+  vlk_buffer_t res = vlk_create_host_buffer(8192, 0);
+
+  uint16_t * ids;
+  _(vkMapMemory(vlk_dev, res.mem, 0, VK_WHOLE_SIZE, 0, (void **)&ids));
+
+  *sz = tkn_encode(ids, txt);
+
+  vkUnmapMemory(vlk_dev, res.mem);
+
+  return res;
+}
+static void dump_output(vlk_buffer_t b, unsigned sz) {
+  uint16_t * ids;
+  _(vkMapMemory(vlk_dev, b.mem, 0, VK_WHOLE_SIZE, 0, (void **)&ids));
+
+  char * buf = mem_alloc(10240);
+  int n = tkn_decode(ids, sz, buf, 10240);
+  if (n) puts(buf);
+
+  vkUnmapMemory(vlk_dev, b.mem);
+}
+
 int main() {
   byt_init();
   bpe_init();
@@ -10,11 +33,10 @@ int main() {
   vcb_init();
   vlk_init();
 
-  tkn_ids_t msg = tkn_encode(txt);
+  unsigned tksz = 0;
+  vlk_buffer_t b_input = load_input(txt, &tksz);
 
-  char * buf = mem_alloc(10240);
-  int n = tkn_decode(msg, buf, 10240);
-  if (n) puts(buf);
+  dump_output(b_input, tksz);
 
   vlk_deinit();
   mem_deinit();
